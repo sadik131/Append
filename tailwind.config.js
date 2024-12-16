8f5f0b951ae82c1852f37d21b33bc3eec29d2a78
supto <salatussadik131@gmail.com>
2024-12-17 00:41:26 +0530

    complite

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,27 @@ module.exports = {
   },
   theme: {
     extend: {
+      boxShadow: {
+        box: "0 4px 16px rgba(0, 0, 0, 0.1)"
+      },
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
       colors: {
+        customMix2: 'color-mix(in srgb, #212529, transparent 20%)',
+        customMix3: 'color-mix(in srgb, #212529, transparent 30%)',
+        customMix4: 'color-mix(in srgb, #212529, transparent 40%)',
+        customMix5: 'color-mix(in srgb, #212529, transparent 50%)',
+        customMix6: 'color-mix(in srgb, #212529, transparent 60%)',
+        customMix7: 'color-mix(in srgb, #212529, transparent 70%)',
+        customMix8: 'color-mix(in srgb, #212529, transparent 80%)',
+        customMix95: 'color-mix(in srgb, #212529, transparent 95%)',
         background: '#ffffff',
         default: '#212529',
         heading: '#32353a',
-        link:"#3a3939",
+        link: "#3a3939",
         primary: '#e84545',
         surface: '#ffffff',
         contrast: '#ffffff',
